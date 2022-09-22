94778963b14089e2599ed2838cb9b6c784f83f57
AmazonPython <amazon3@foxmail.com>
2022-09-22 21:30:22 +0800

    Update homepage style. 💄

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,49 @@
       <div class="banner">
         <img class="banner__img" src="http://www.dell-lee.com/imgs/vue3/banner.jpg" alt="banner" />
       </div>
+      <div class="icons">
+        <div class="icons__item">
+          <img class="icons__item__img" src="http://www.dell-lee.com/imgs/vue3/超市.png" alt="超市" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="http://www.dell-lee.com/imgs/vue3/菜市场.png" alt="菜市场" />
+          <p class="icons__item__desc">菜市场</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="http://www.dell-lee.com/imgs/vue3/超市.png" alt="超市" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="http://www.dell-lee.com/imgs/vue3/超市.png" alt="超市" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="http://www.dell-lee.com/imgs/vue3/超市.png" alt="超市" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="http://www.dell-lee.com/imgs/vue3/超市.png" alt="超市" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="http://www.dell-lee.com/imgs/vue3/超市.png" alt="超市" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="http://www.dell-lee.com/imgs/vue3/超市.png" alt="超市" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="http://www.dell-lee.com/imgs/vue3/超市.png" alt="超市" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="http://www.dell-lee.com/imgs/vue3/超市.png" alt="超市" />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+      </div>
+      <div class="gap"></div>
     </div>
     <div class="docker">
       <div class="docker__item docker__item--active">
@@ -81,7 +124,6 @@
     font-size: .14rem;
   }
 }
-
 .banner {
   height: 0;
   overflow: hidden;
@@ -90,7 +132,30 @@
     width: 100%;
   }
 }
-
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: .16rem;
+  &__item {
+    width: 20%;
+    &__img {
+      display:block;
+      width: .4rem;
+      height: .4rem;
+      margin: 0 auto;
+    }
+    &__desc {
+      margin: .06rem 0 .16rem 0;
+      text-align: center;
+      color: $content-fontcolor;
+    }
+  }
+}
+.gap {
+  margin: 0 -.18rem;
+  height: .1rem;
+  background: $content-bgColor;
+}
 .docker {
   display: flex;
   box-sizing: border-box;
@@ -100,7 +165,7 @@
   bottom: 0;
   width: 100%;
   height: .49rem;
-  border-top: .01rem solid #F1F1F1;
+  border-top: .01rem solid $content-bgColor;
   color: $content-fontcolor;
   &__item {
     flex: 1;
