3108006a0a298a85db1cb547d37c8f7cf7a20dc9
AmazonPython <amazon3@foxmail.com>
2022-11-06 17:20:08 +0800

    Complete the logical development of the order list. 💄 👔 🎨

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="wrapper">
-    <StaticPart />
-    <Nearby />
+  <div className="wrapper">
+    <StaticPart/>
+    <Nearby/>
   </div>
-  <Docker />
+  <Docker :currentIndex="0"/>
 </template>
 
 <script>
 import StaticPart from './StaticPart'
 import Nearby from './Nearby'
-import Docker from './Docker'
+import Docker from '../../components/Docker'
+
 export default {
   name: 'Home',
-  components: { StaticPart, Nearby, Docker }
+  components: {StaticPart, Nearby, Docker}
 }
 </script>
 
